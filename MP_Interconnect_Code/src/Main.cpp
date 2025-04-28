5d7c4e3f1c60f384910be603cdbc3ca0cf6b9c75
DanielMontoyaR <danimo.rivera@gmail.com>
2025-04-27 21:32:13 -0600

    INV_ACK INV_COMPLETE READ_RESP and WRITE_RESP functions implementation. Object Interconnect implementation (unused until full integration)

--- MP_Interconnect_Code/src/Main.cpp
+++ MP_Interconnect_Code/src/Main.cpp
@@ -42,37 +42,286 @@ std::mutex pe7_cache_mtx;
 std::mutex* pe_cache_mutexes[] = {&pe0_cache_mtx, &pe1_cache_mtx, &pe2_cache_mtx, &pe3_cache_mtx, &pe4_cache_mtx, &pe5_cache_mtx, &pe6_cache_mtx, &pe7_cache_mtx};
 
 
+
+class Interconnect{
+    public:
+
+    void write_mem(uint8_t src, uint32_t addr, uint16_t num_of_cache_lines, uint16_t start_cache_line, uint16_t qos){
+        cout << "Entrada al write_mem desde el objeto Interconnect" << "\n\n\n\n" << endl;
+    
+        int shared_row = addr / 32;
+        int shared_col = addr % 32;
+    
+        cout << "El valor de fila y columna con la dirección " << std::hex << addr << " son: \nfila: "<< std::dec << shared_row << "\ncolumna: " << shared_col << endl << endl;
+        
+        //std::lock(shared_memory_mtx,pe0_cache_mtx,pe1_cache_mtx);
+    
+        //shared_memory_mtx.lock();
+    
+        std::lock(shared_memory_mtx,(*pe_cache_mutexes[src]));
+    
+        //int cache_col = 1;
+    
+        for(int cache_row = 0; cache_row < num_of_cache_lines; cache_row++){ //Loop for traverse the number of cache lines
+    
+            if(pe_caches[src]->at(start_cache_line).at(0) == 1){
+                cout << "Error, line " << start_cache_line << " of PE "<< src*1 <<" is invalid" << endl; //We evaluate the invalidation bit
+                start_cache_line++;
+            }
+            else{
+                
+                for(int cache_col=1; cache_col<(*pe_caches[src])[0].size(); cache_col++){
+    
+                    shared_memory[shared_row][shared_col] = (*pe_caches[src])[start_cache_line][cache_col];//This one does the same as the first one
+                    shared_col++;
+    
+                    if(shared_col==shared_memory[0].size()){
+                        
+                        shared_col=0;
+                        shared_row++;
+                    }
+                }
+                start_cache_line++;
+            }
+        }
+    
+        shared_memory_mtx.unlock();
+        (*pe_cache_mutexes[src]).unlock();
+    
+    
+        return;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    void read_mem(uint8_t src, uint32_t addr, uint32_t size, uint16_t qos){
+        cout << "Entrada al read_mem" << "\n\n\n\n" << endl;
+        //src = src*1;
+        
+        int shared_row = addr / 32;
+        int shared_col = addr % 32;
+    
+        int cache_row = addr / 128;
+        //int cache_row = std::floor(addr/128);
+        int cache_col = addr % 128;
+    
+        cout << "Cache row value is: " << cache_row << endl;
+    
+        cout << "Shared row value is: " << shared_row << endl; 
+    
+        int byte_to_bit = int(size)*8;
+    
+    
+    
+        cout << "The size in bytes is: " << std::hex << size << ". Its equivalent in decimal is: " << std::dec << byte_to_bit << endl;
+    
+        std::lock(shared_memory_mtx,(*pe_cache_mutexes[src]));
+    
+        if(cache_col == 0){ //Cache col 0 is always used for the invalidation bit
+            cache_col = 1; 
+        }
+    
+        for(int bits = 0; bits < byte_to_bit; bits++){
+    
+    
+            (*pe_caches[src])[cache_row][cache_col] = shared_memory[shared_row][shared_col];
+    
+            shared_col++;
+            cache_col++;
+    
+            if(shared_col = shared_memory[0].size()){//End shared memory columns
+    
+                shared_row++;
+                shared_col=0;
+    
+            }
+            else if (cache_col == (*pe_caches[src])[0].size()){// End cache memory columns
+    
+                cache_row++;
+                cache_col=1;
+    
+            }
+            
+    
+        }
+    
+    
+        shared_memory_mtx.unlock();
+        (*pe_cache_mutexes[src]).unlock();
+    
+        return;
+    }
+    
+    
+    
+    
+    void inv_complete(uint8_t dest, uint16_t qos){
+        //cout << "Entrada al inv_complete" << "\n\n\n\n" << endl;
+    
+        cout << "[PE" << dest*1 << "] → INV_COMPLETE: Invalidation Process Completed"<<endl;
+    
+    }
+    
+    
+    void inv_ack(uint8_t src, uint16_t cache_line, uint16_t qos){
+    
+        //cout << "Entrada al inv_ack" << "\n\n\n\n" << endl;
+    
+        for(int PEs = 0; PEs < 8; PEs++){
+    
+            if(pe_caches[PEs]->at(cache_line).at(0) == 1){
+    
+                cout << "[PE"<< src*1 << "] → [PE" << unsigned(PEs) << "] INV_ACK: Invalidation Bit Confirmed at line " << cache_line <<" (QoS=" << qos << ")\n";
+    
+            }
+            else{
+    
+                cout << "[PE"<< src*1 << "] → [PE" << unsigned(PEs) << "] INV_ACK: Error, the bit was not invalidated at line " << cache_line <<" (QoS=" << qos << ")\n";
+    
+            }
+        }
+    
+    
+        
+        return;
+    
+    }
+    
+    
+    void broadcast_invalidate(uint8_t src, uint16_t cache_line, uint16_t qos){
+        cout << "Entrada al broadcast_invalidate" << "\n\n\n\n" << endl;
+    
+        std::lock(pe0_cache_mtx, pe1_cache_mtx, pe2_cache_mtx, pe3_cache_mtx, pe4_cache_mtx, pe5_cache_mtx, pe6_cache_mtx, pe7_cache_mtx);
+    
+        for(int PEs = 0; PEs < 8; PEs++){
+    
+            pe_caches[PEs]->at(cache_line).at(0) = 1;
+            
+    
+        }
+        inv_ack(src, cache_line, qos);
+    
+        pe0_cache_mtx.unlock();
+        pe1_cache_mtx.unlock();
+        pe2_cache_mtx.unlock();
+        pe3_cache_mtx.unlock();
+        pe4_cache_mtx.unlock();
+        pe5_cache_mtx.unlock();
+        pe6_cache_mtx.unlock();
+        pe7_cache_mtx.unlock();
+    
+        inv_complete(src, qos);
+    
+        return;
+    }
+    
+    
+    
+    
+    
+    void read_resp(uint8_t dest, std::array<uint32_t, 4096> data, uint16_t qos){
+        cout << "Entrada al read_resp" << "\n\n\n\n" << endl;
+    }
+    
+    void write_resp(uint8_t dest, uint8_t status, uint16_t qos){
+        cout << "Entrada al write_resp" << "\n\n\n\n" << endl;
+    }
+    
+
+
+};//End Interconnect class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void write_resp(uint8_t dest, uint8_t status, uint16_t qos){
+    //cout << "[PE"<< dest*1 << "] → WRITE_RESP: Process Initiated" << endl;
+
+    if(status == 1){
+        cout << "[PE"<< dest*1 << "] → WRITE_RESP: Write Operation Successful.  System exit with STATUS = " << status*1 << endl;
+    }
+    else{
+        cout << "[PE"<< dest*1 << "] → WRITE_RESP: Write Operation Unsuccessful. System exit with STATUS = " << status*1 << endl;
+    }
+    return;
+}
+
+
 void write_mem(uint8_t src, uint32_t addr, uint16_t num_of_cache_lines, uint16_t start_cache_line, uint16_t qos){
-    //cout << std::hex << "Entrada al write_mem con los parámetros \n src " << src*1 << "\n addr " << addr << "\n num_of_cache_lines " << num_of_cache_lines 
-    //<< "\n start_cache_line " << start_cache_line << "\n qos " << qos << "\n\n\n\n" << endl;
-    //int memory_adress = int(addr);
-    //cout << "La dirección es: " << memory_adress << endl;
+
+    cout << "[PE"<< src*1 << "] → WRITE_MEM: Process Initiated" << endl;
 
     int shared_row = addr / 32;
     int shared_col = addr % 32;
 
-    cout << "El valor de fila y columna con la dirección " << std::hex << addr << " son: \nfila: "<< std::dec << shared_row << "\ncolumna: " << shared_col << endl << endl;
+    //cout << "El valor de fila y columna con la dirección " << std::hex << addr << " son: \nfila: "<< std::dec << shared_row << "\ncolumna: " << shared_col << endl << endl;
     
     //std::lock(shared_memory_mtx,pe0_cache_mtx,pe1_cache_mtx);
 
-    shared_memory_mtx.lock();
+    //shared_memory_mtx.lock();
+
+    std::lock(shared_memory_mtx,(*pe_cache_mutexes[src]));
 
     //int cache_col = 1;
 
-    for(int cache_row = 0; cache_row < num_of_cache_lines; cache_row++){ //Loop for traverse the number of cache lines
+    uint8_t status = 0x1;
 
-        if(pe_caches[src]->at(start_cache_line).at(0) == 1){
-            cout << "Error, line " << start_cache_line << " of PE "<< src*1 <<" is invalid" << endl; //We evaluate the invalidation bit
-            start_cache_line++;
+    int cache_row = start_cache_line;
+
+    for(int cache_lines = 0; cache_lines < num_of_cache_lines; cache_lines++){
+
+        //cout << "row " << cache_row << " col " << "0 " << "Value " << (*pe_caches[src])[cache_row][0]*1 << endl;
+
+        if((*pe_caches[src])[cache_row][0] == 1){
+
+            cout << "[PE" << src*1 << "] → WRITE_MEM: Error, cache line " << cache_row << " is invalid" << endl; //We evaluate the invalidation bit
+            status = 0x0;   
+
         }
-        else{
-            
-            for(int cache_col=1; cache_col<(*pe_caches[src])[0].size(); cache_col++){
-                //cout << "Wrote on line blah blah this " << pe_caches[src]->at(start_cache_line).at(cache_col)*1 << endl;
-                //shared_memory[shared_row][shared_col] = pe_caches[src]->at(start_cache_line).at(cache_col);
-                shared_memory[shared_row][shared_col] = (*pe_caches[src])[start_cache_line][cache_col];//This one does the same as the first one
-                shared_col++;
+        cache_row++;
+    }
 
+
+    if(status == 0x1){
+        for(int cache_row = 0; cache_row < num_of_cache_lines; cache_row++){ //Loop for traverse the number of cache lines
+ 
+            for(int cache_col=1; cache_col<(*pe_caches[src])[0].size(); cache_col++){
+    
+                shared_memory[shared_row][shared_col] = (*pe_caches[src])[start_cache_line][cache_col];//Operation to write on memory
+                shared_col++;
+    
                 if(shared_col==shared_memory[0].size()){
                     
                     shared_col=0;
@@ -83,7 +332,11 @@ void write_mem(uint8_t src, uint32_t addr, uint16_t num_of_cache_lines, uint16_t
         }
     }
 
+
+    write_resp(src,status,qos);
+
     shared_memory_mtx.unlock();
+    (*pe_cache_mutexes[src]).unlock();
 
 
     return;
@@ -95,9 +348,26 @@ void write_mem(uint8_t src, uint32_t addr, uint16_t num_of_cache_lines, uint16_t
 
 
 
+void read_resp(uint8_t dest, std::vector<uint8_t> data, uint16_t qos){
+    
+    cout << "[PE"<< dest*1 << "] → READ_RESP: Process Initiated" << endl;
+
+
+    
+    cout <<"[PE" << dest*1 << "] → READ_RESP:"<< "Data wrote to cache memory:" << endl;
+    for(int data_index = 0; data_index < data.size(); data_index++){
+        cout << data[data_index]*1; 
+    }
+
+    cout << endl;
+
+}
+
 
 void read_mem(uint8_t src, uint32_t addr, uint32_t size, uint16_t qos){
-    cout << "Entrada al read_mem" << "\n\n\n\n" << endl;
+
+    cout << "[PE"<< src*1 << "] → READ_MEM: Process Initiated" << endl;
+
     //src = src*1;
     
     int shared_row = addr / 32;
@@ -107,15 +377,19 @@ void read_mem(uint8_t src, uint32_t addr, uint32_t size, uint16_t qos){
     //int cache_row = std::floor(addr/128);
     int cache_col = addr % 128;
 
-    cout << "Cache row value is: " << cache_row << endl;
+    //cout << "Cache row value is: " << cache_row << endl;
 
-    cout << "Shared row value is: " << shared_row << endl; 
+    //cout << "Shared row value is: " << shared_row << endl;
 
     int byte_to_bit = int(size)*8;
 
-
+    std::vector<uint8_t> data(byte_to_bit);
 
     cout << "The size in bytes is: " << std::hex << size << ". Its equivalent in decimal is: " << std::dec << byte_to_bit << endl;
+
+
+
+
 
     std::lock(shared_memory_mtx,(*pe_cache_mutexes[src]));
 
@@ -125,29 +399,38 @@ void read_mem(uint8_t src, uint32_t addr, uint32_t size, uint16_t qos){
 
     for(int bits = 0; bits < byte_to_bit; bits++){
 
+        if((*pe_caches[src])[cache_row][0] == 1){
+            (*pe_caches[src])[cache_row][0] = 0; //We validate the bit here because we brought an updated block of memory
+        }
 
         (*pe_caches[src])[cache_row][cache_col] = shared_memory[shared_row][shared_col];
+
+        data[bits] =(*pe_caches[src])[cache_row][cache_col];
+        //cout << "Wrote value:" << shared_memory[shared_row][shared_col]*1 << "\nrow: " << shared_row << "\ncol:"<< shared_col <<endl;
 
         shared_col++;
         cache_col++;
 
-        if(shared_col = shared_memory[0].size()){//End shared memory columns
+        if(shared_col == shared_memory[0].size()){//End shared memory columns
 
             shared_row++;
             shared_col=0;
 
         }
-        else if (cache_col == (*pe_caches[src])[0].size()){// End cache memory columns
+        
+        if (cache_col == (*pe_caches[src])[0].size()){// End cache memory columns
+
+
 
             cache_row++;
             cache_col=1;
 
         }
         
-        
 
     }
 
+    read_resp(src, data, qos);
 
     shared_memory_mtx.unlock();
     (*pe_cache_mutexes[src]).unlock();
@@ -155,16 +438,55 @@ void read_mem(uint8_t src, uint32_t addr, uint32_t size, uint16_t qos){
     return;
 }
 
+
+
+
+void inv_complete(uint8_t dest, uint16_t qos){
+    //cout << "Entrada al inv_complete" << "\n\n\n\n" << endl;
+    
+
+    cout << "[PE" << dest*1 << "] → INV_COMPLETE: Invalidation Process Completed"<<endl;
+
+}
+
+
+void inv_ack(uint8_t src, uint16_t cache_line, uint16_t qos){
+
+    cout << "[PE"<< src*1 << "] → INV_ACK: Process Initiated" << endl;
+
+    for(int PEs = 0; PEs < 8; PEs++){
+
+        if(pe_caches[PEs]->at(cache_line).at(0) == 1){
+
+            cout << "[PE"<< src*1 << "] → [PE" << unsigned(PEs) << "] INV_ACK: Invalidation Bit Confirmed at line " << cache_line <<" (QoS=" << qos << ")" << endl;
+
+        }
+        else{
+
+            cout << "[PE"<< src*1 << "] → [PE" << unsigned(PEs) << "] INV_ACK: Error, the bit was not invalidated at line " << cache_line <<" (QoS=" << qos << ")"<< endl;
+
+        }
+    }
+
+
+    
+    return;
+
+}
+
+
 void broadcast_invalidate(uint8_t src, uint16_t cache_line, uint16_t qos){
-    //cout << "Entrada al broadcast_invalidate" << "\n\n\n\n" << endl;
+    cout << "[PE"<< src*1 << "] → BROADCAST_INVALIDATE: Process Initiated" << endl;
 
     std::lock(pe0_cache_mtx, pe1_cache_mtx, pe2_cache_mtx, pe3_cache_mtx, pe4_cache_mtx, pe5_cache_mtx, pe6_cache_mtx, pe7_cache_mtx);
 
     for(int PEs = 0; PEs < 8; PEs++){
 
-        pe_caches[PEs]->at(cache_line-1).at(0) = 1;
+        pe_caches[PEs]->at(cache_line).at(0) = 1;
+        
 
     }
+    inv_ack(src, cache_line, qos);
 
     pe0_cache_mtx.unlock();
     pe1_cache_mtx.unlock();
@@ -175,30 +497,24 @@ void broadcast_invalidate(uint8_t src, uint16_t cache_line, uint16_t qos){
     pe6_cache_mtx.unlock();
     pe7_cache_mtx.unlock();
 
+    inv_complete(src, qos);
+
     return;
 }
 
-void inv_ack(uint8_t src, uint16_t qos){
-    cout << "Entrada al inv_ack" << "\n\n\n\n" << endl;
-}
-
-void inv_complete(uint8_t dest, uint16_t qos){
-    cout << "Entrada al inv_complete" << "\n\n\n\n" << endl;
-}
-
-void read_resp(uint8_t dest, std::array<uint32_t, 4096> data, uint16_t qos){
-    cout << "Entrada al read_resp" << "\n\n\n\n" << endl;
-}
-
-void write_resp(uint8_t dest, uint8_t status, uint16_t qos){
-    cout << "Entrada al write_resp" << "\n\n\n\n" << endl;
-}
 
 
 
-void instructionReader(uint8_t src){
 
-    uint16_t qos = 0; // For testing until we implement qos.
+
+
+
+
+void instructionReader(uint8_t src, uint16_t qos){
+
+    //uint16_t qos = 0; // For testing until we implement qos.
+
+    Interconnect bus_interconnect;
 
     string filename = "../Workloads/PE" + to_string(src) + "_Instructions.txt";
 
@@ -235,6 +551,7 @@ void instructionReader(uint8_t src){
             uint16_t start_cache_line = static_cast<uint16_t>(stoul(start_line_str, nullptr, 0));
             
             write_mem(src, addr, num_of_cache_lines, start_cache_line, qos);
+            //bus_interconnect.write_mem(src, addr, num_of_cache_lines, start_cache_line, qos);
         } 
         else if (instruction == "READ_MEM") {
             string addr_str, size_str;
@@ -274,10 +591,15 @@ int main() {
 
     //First bit of each pe_cache is the invalidation bit
     shared_memory[0][0] = 1;
-    shared_memory[0][1] = 0;
-    shared_memory[0][2] = 1;
-    shared_memory[0][3] = 1;
-    pe0_cache[0][0] = 0; //Here we disable the first line.
+    shared_memory[0][1] = 2;
+    shared_memory[0][2] = 3;
+    shared_memory[0][3] = 4;
+    shared_memory[0][4] = 5;
+    shared_memory[0][5] = 6;
+    shared_memory[0][6] = 7;
+    shared_memory[0][7] = 8;
+
+    pe0_cache[0][0] = 1; //Here we disable the first line.
     pe0_cache[1][1] = 0; //Here we don't disable the line
 
     for(int row=0; row<pe0_cache.size();row++){
@@ -290,10 +612,11 @@ int main() {
     pe0_cache[1][0]= 0;
 
 
-
+    uint8_t src = 0;
+    uint16_t qos = 0x00;
 
     //Here we use threads (of for each PE)
-    instructionReader(uint8_t(0));
+    instructionReader(src,qos);
 
     //cout << "El tamaño del cache es: " << pe0_cache[0].size() <<endl;
     //pe0_cache[0][pe0_cache[0].size()] = 7;
@@ -306,6 +629,8 @@ int main() {
         savePECacheToFile(i,*pe_caches[i]);
     }
 
+
+    cout << "All operations completed, program finished." << endl;
     return 0;
 }
 
